99a1a49f132aeef2d8a46b9eac52ca12319c6686
criztiandev <criztiandev@gmail.com>
2023-03-03 16:17:03 +0800

    Final

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MotionSlider from "./components/MotionSlider/MotionSlider";
+
+function App() {
+  return (
+    <div className="App">
+      <MotionSlider />
+    </div>
+  );
+}
+
+export default App;
